cc5e3101f2119e757df81ebd1b79a1da4232f1b5
Blagoj <blagoj.petrov5@gmail.com>
2022-02-14 23:45:00 +0100

    Add security middlewares and cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import express from 'express';
 import { main } from './main';
+import helmet from 'helmet';
+import cors from 'cors';
+import morgan from 'morgan';
 
 const app = express();
 
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(morgan('combined'));
 
 app.post('/analize', async function (req, res) {
   const { body } = req;
